14456f3a3cc5d6c33554828b9c39a15ba32703b8
Luna <luna@chloedev.de>
2025-03-11 03:16:03 +0100

    Automatically create release on build

---
+++ create-release.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+REM Set the zip file name and path
+set ZIP_FILE=release\PlayerVoiceEvents.zip
+
+REM Create the release directory if it doesn't exist
+if not exist release (
+    mkdir release
+)
+
+REM Remove the existing zip file if it exists
+if exist %ZIP_FILE% del /f %ZIP_FILE%
+
+REM Create the zip file and include the specified files and directories
+powershell -Command "Compress-Archive -Path .\Scripts\, .\Seq\, .\SKSE\, .\.LNTC_PVE_DEV_MODE, .\LICENSE, .\LNTC_PVE_Event_List.jsonc, .\PlayerVoiceEvents.esp -DestinationPath %ZIP_FILE%"
+
+echo Created Release at: "%ZIP_FILE%"
+
+endlocal
