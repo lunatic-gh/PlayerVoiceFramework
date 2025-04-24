243aa367565423d45a4b303d1617e25430720118
luna <luna@chloedev.de>
2025-04-24 17:01:26 +0200

    Fix build

--- cmake/ports/commonlibsse-ng/portfile.cmake
+++ cmake/ports/commonlibsse-ng/portfile.cmake
@@ -1,23 +1,34 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
-        REPO CharmedBaryon/CommonLibSSE
-        REF b93280e832f263dbef44e44cbe2936622a02f91a
-        SHA512 c98a0dde8fab45d0b5ffc8241bc6437fc1b2855e5577e9b66fe4e237c35c7111c3288f0ef7a637b2a399dd38938221619331e3b49504b76ac0e1a0a2034715a6
-        HEAD_REF main
+        REPO alandtse/CommonLibVR
+        REF 539d4ce50969e8d23f4d507a7bc3dd955a4de93f
+        SHA512 7353f26b3abb0f6202d9283094ccaf986f1354359527cd44dbefab6b0bc7cb21aed678c1b7460fddc344add3aec1d5de0cb576efcfc788bb7b774a280abfe7a1
+        HEAD_REF ng
 )
+
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH2
+        REPO ValveSoftware/openvr
+        REF ebdea152f8aac77e9a6db29682b81d762159df7e
+        SHA512 4fb668d933ac5b73eb4e97eb29816176e500a4eaebe2480cd0411c95edfb713d58312036f15db50884a2ef5f4ca44859e108dec2b982af9163cefcfc02531f63
+        HEAD_REF master
+)
+
+file(GLOB OPENVR_FILES "${SOURCE_PATH2}/*")
+
+file(COPY ${OPENVR_FILES} DESTINATION "${SOURCE_PATH}/extern/openvr")
 
 vcpkg_configure_cmake(
         SOURCE_PATH "${SOURCE_PATH}"
         PREFER_NINJA
         OPTIONS -DBUILD_TESTS=off -DSKSE_SUPPORT_XBYAK=on
-        MAYBE_UNUSED_VARIABLES
-        -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 )
 
 vcpkg_install_cmake()
 vcpkg_cmake_config_fixup(PACKAGE_NAME CommonLibSSE CONFIG_PATH lib/cmake)
 vcpkg_copy_pdbs()
 
+file(INSTALL "${SOURCE_PATH2}/headers/openvr.h" DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 file(GLOB CMAKE_CONFIGS "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE/CommonLibSSE/*.cmake")
 file(INSTALL ${CMAKE_CONFIGS} DESTINATION "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE")
 file(INSTALL "${SOURCE_PATH}/cmake/CommonLibSSE.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE")
